2f67e50ded999bc263aab35b3d8003e8f724ee10
Venyl <zexe2121@gmail.com>
2022-08-05 16:49:53 +0200

    Edit error message

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 const { DISCORD_TOKEN, CLIENT_ID, GUIlD_ID } = process.env;
 if (!DISCORD_TOKEN || !CLIENT_ID || !GUIlD_ID)
-    throw new Error(`Missing environment variable, env: ${process.env}`);
+    throw new Error(
+        `Missing environment variable, env: ${process.env.DISCORD_TOKEN}, ${process.env.CLIENT_ID}, ${process.env.GUILD_ID}`
+    );
 
 const config: Record<string, string> = {
     DISCORD_TOKEN,
