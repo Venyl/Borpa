b4621bde018c11ad02be2b543d3734c71dd036ae
Venyl <zexe2121@gmail.com>
2022-08-05 16:57:17 +0200

    Add 2nd fix for config

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,15 @@
 import dotenv from 'dotenv';
 dotenv.config();
-const { DISCORD_TOKEN, CLIENT_ID, GUIlD_ID } = process.env;
-// if (!process.env.DISCORD_TOKEN || !process.env.CLIENT_ID || !process.env.GUIlD_ID)
-//     throw new Error(
-//         `Missing environment variable, env: ${process.env.DISCORD_TOKEN}, ${process.env.CLIENT_ID}, ${process.env.GUILD_ID}`
-//     );
+const { DISCORD_TOKEN, CLIENT_ID, GUIlD_ID: GUILD_ID } = process.env;
+if (!process.env.DISCORD_TOKEN || !process.env.CLIENT_ID || !process.env.GUIlD_ID)
+    throw new Error(
+        `Missing environment variable, env: ${DISCORD_TOKEN}, ${CLIENT_ID}, ${GUILD_ID}`
+    );
 
-const config: Record<string, string | undefined> = {
-    DISCORD_TOKEN,
-    CLIENT_ID,
-    GUIlD_ID,
+const config: Record<string, string> = {
+    DISCORD_TOKEN: DISCORD_TOKEN!,
+    CLIENT_ID: CLIENT_ID!,
+    GUIlD_ID: GUILD_ID!,
 };
 
 export default config;
